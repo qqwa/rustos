c97df404665e4b6470e08a896b5c4efd8aef7bf5
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2018-06-08 14:13:43 +0200

    variables from linker script now have prefix __
    sp.s no directly references the __stack_top variable

--- src/sp.s
+++ src/sp.s
@@ -1,8 +1,8 @@
-// .extern stack_pointer
 .weak   _RESET
+.extern __stack_top
 .section ".text.RESET","aw"
 _RESET:
-    adrp    x8, :got:stack_top		// defined by the linker script
+    adrp    x8, __stack_top		// defined by the linker script
     mov sp, x8
     bl rust_entry
     b .
