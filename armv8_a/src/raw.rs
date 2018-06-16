fb7555663074ea2a69868bf048622c31ec0a01bc
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2018-06-16 14:48:59 +0200

    added special-purpose registers to armv8_a crate

--- armv8_a/src/raw.rs
+++ armv8_a/src/raw.rs
@@ -1,6 +1,10 @@
 /// Read Register
 macro_rules! rreg {
-    ($function:ident, $type:ty, $instruction:expr, $register:expr) => {
+    (
+        $(#[$m:meta])*
+        $function:ident, $type:ty, $instruction:expr, $register:expr
+    ) => {
+        $(#[$m])*
         #[inline(always)]
         pub unsafe fn $function() -> $type
         {
@@ -13,7 +17,11 @@ macro_rules! rreg {
 
 /// Write Register
 macro_rules! wreg {
-    ($function:ident, $type:ty, $instruction:expr, $register:expr) => {
+    (
+        $(#[$m:meta])*
+        $function:ident, $type:ty, $instruction:expr, $register:expr
+    ) => {
+        $(#[$m])*
         #[inline(always)]
         pub unsafe fn $function(val: $type) {
            asm!(concat!($instruction, " ", $register, ", $0")::"r"(val)::"volatile");
@@ -30,23 +38,203 @@ macro_rules! instr {
     }
 }
 
-/// Condition Flags
-rreg!(get_nzcv, u32, "mrs", "NZCV");
-wreg!(set_nzcv, u32, "msr", "NZCV");
+rreg!(
+    /// determine the current Exception level
+    get_current_el, u32, "mrs", "CurrentEL"
+);
 
-/// Interrupt Mask Bits
-rreg!(get_daif, u32, "mrs", "DAIF");
+rreg!(
+    /// {D, A, I, F} interrupt mask bits
+    get_daif, u32, "mrs", "DAIF"
+);
 
-/// Current Exception Level
-rreg!(get_current_el, u32, "mrs", "CurrentEL");
+rreg!(
+    /// get the address to return to for an exception return from EL1
+    get_elr_el1, usize, "mrs", "ELR_EL1"
+);
+wreg!(
+    /// set the address to return to for an exception return from EL1
+    set_elr_el1, usize, "msr", "ELR_EL1"
+);
 
-/// Stack Pointer Select
-rreg!(get_sp_sel, u32, "mrs", "SPSel");
-wreg!(set_sp_sel, u32, "msr", "SPSel");
+rreg!(
+    /// get the address to return to for an exception return from EL2
+    get_elr_el2, usize, "mrs", "ELR_EL2"
+);
+wreg!(
+    /// set the address to return to for an exception return from EL2
+    set_elr_el2, usize, "msr", "ELR_EL2"
+);
 
-// /// Privileged Access Never
-// rreg!(get_pan, u32, "mrs", "PAN");
+rreg!(
+    /// set the address to return to for an exception return from EL3
+    get_elr_el3, usize, "mrs", "ELR_EL3"
+);
+wreg!(
+    /// get the address to return to for an exception return from EL3
+    set_elr_el3, usize, "msr", "ELR_EL3"
+);
 
-// /// User Access Override
-// rreg!(get_uao, u32, "mrs", "UAO");
+rreg!(
+    /// provides control of floating-point operation
+    set_fpcr, u32, "mrs", "FPCR"
+);
+wreg!(
+    /// provides control of floating-point operation
+    get_fpcr, u32, "msr", "FPCR"
+);
 
+rreg!(
+    /// provides floating-point status information
+    set_fpsr, u32, "mrs", "FPSR"
+);
+wreg!(
+    /// provides floating-point status information
+    get_fpsr, u32, "msr", "FPSR"
+);
+
+rreg!(
+    /// {N, Z, C, V} condition flags
+    get_nzcv, u32, "mrs", "NZCV"
+);
+wreg!(
+    /// {N, Z, C, V} condition flags
+    set_nzcv, u32, "msr", "NZCV"
+);
+
+#[cfg(any(target_feature = "v8.1a", target_feature = "v8.2a", target_feature = "v8.3a"))]
+rreg!(
+    /// Privileged Access Never bit
+    get_pan, u32, "mrs", "PAN"
+);
+#[cfg(any(target_feature = "v8.1a", target_feature = "v8.2a", target_feature = "v8.3a"))]
+wreg!(
+    /// Privileged Access Never bit
+    set_pan, u32, "msr", "PAN"
+);
+
+rreg!(
+    /// holds the stack pointer for EL0
+    get_sp_el0, usize, "mrs", "SP_EL0"
+);
+wreg!(
+    /// sets the stack pointer for EL0
+    set_sp_el0, usize, "msr", "SP_EL0"
+);
+
+rreg!(
+    /// holds the stack pointer for EL1
+    get_sp_el1, usize, "mrs", "SP_EL1"
+);
+wreg!(
+    /// sets the stack pointer for EL1
+    set_sp_el1, usize, "msr", "SP_EL1"
+);
+
+rreg!(
+    /// holds the stack pointer for EL2
+    get_sp_el2, usize, "mrs", "SP_EL2"
+);
+wreg!(
+    /// sets the stack pointer for EL2
+    set_sp_el2, usize, "msr", "SP_EL2"
+);
+
+rreg!(
+    /// holds the stack pointer for EL3
+    get_sp_el3, usize, "mrs", "SP_EL3"
+);
+wreg!(
+    /// sets the stack pointer for EL3
+    set_sp_el3, usize, "msr", "SP_EL3"
+);
+
+rreg!(
+    /// Stack Pointer Select between SP_EL0 and SP_ELx
+    get_sp_sel, u32, "mrs", "SPSel"
+);
+wreg!(
+    /// Stack Pointer Select between SP_EL0 and SP_ELx
+    set_sp_sel, u32, "msr", "SPSel"
+);
+
+rreg!(
+    /// holds process state on taking an exception to AArch32 Abort mode
+    get_spsr_abt, u32, "mrs", "SPSR_abt"
+);
+wreg!(
+    /// holds process state on taking an exception to AArch32 Abort mode
+    set_spsr_abt, u32, "msr", "SPSR_abt"
+);
+
+rreg!(
+    /// holds process state on taking an exception to AArch64 EL1
+    get_spsr_el1, u32, "mrs", "SPSR_EL1"
+);
+wreg!(
+    /// holds process state on taking an exception to AArch64 EL1
+    set_spsr_el1, u32, "msr", "SPSR_EL1"
+);
+
+rreg!(
+    /// holds process state on taking an exception to AArch64 EL2
+    get_spsr_el2, u32, "mrs", "SPSR_EL2"
+);
+wreg!(
+    /// holds process state on taking an exception to AArch64 EL2
+    set_spsr_el2, u32, "msr", "SPSR_EL2"
+);
+
+rreg!(
+    /// holds process state on taking an exception to AArch64 EL3
+    get_spsr_el3, u32, "mrs", "SPSR_EL3"
+);
+wreg!(
+    /// holds process state on taking an exception to AArch64 EL3
+    set_spsr_el3, u32, "msr", "SPSR_EL3"
+);
+
+rreg!(
+    /// holds process state on taking an exception to AArch32 FIQ mode
+    get_spsr_fiq, u32, "mrs", "SPSR_fiq"
+);
+wreg!(
+    /// holds process state on taking an exception to AArch32 FIQ mode
+    set_spsr_fiq, u32, "msr", "SPSR_fiq"
+);
+
+rreg!(
+    /// holds process state on taking an exception to AArch32 IRQ mode
+    get_spsr_irq, u32, "mrs", "SPSR_irq"
+);
+wreg!(
+    /// holds process state on taking an exception to AArch32 IRQ mode
+    set_spsr_irq, u32, "msr", "SPSR_irq"
+);
+
+rreg!(
+    /// holds process state on taking an exception to AArch32 Undefined mode
+    get_spsr_und, u32, "mrs", "SPSR_und"
+);
+wreg!(
+    /// holds process state on taking an exception to AArch32 Undefined mode
+    set_spsr_und, u32, "msr", "SPSR_und"
+);
+
+rreg!(
+    /// holds the address to return to for a return from Debug state
+    get_dlr_el0, usize, "mrs", "DLR_EL0"
+);
+wreg!(
+    /// holds the address to return to for a return from Debug state
+    set_dlr_el0, usize, "msr", "DLR_EL0"
+);
+
+rreg!(
+    /// holds process state on entry to Debug state
+    get_dspsr_el0, u32, "mrs", "DSPSR_EL0"
+);
+wreg!(
+    /// holds process state on entry to Debug state
+    set_dspsr_el0, u32, "msr", "DSPSR_EL0"
+);
