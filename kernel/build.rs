17d87ed949bb97ab69be1aae299c9a6ab2dd6c8d
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2018-08-15 13:45:29 +0200

    config file, build script, cond compiling

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,5 +1,26 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    println!("cargo:rustc-cfg=device=\"{}\"", env::var("DEVICE").unwrap_or("imx8".to_string()));
+
+    let mut conf_file = File::open("../conf.toml").expect("could not find config file");
+    
+    let mut config = String::new();
+    conf_file.read_to_string(&mut config)
+        .expect("something went wrong reading the file");
+
+    let config: toml::Value = toml::from_str(&config).unwrap();
+
+    for (name, value) in config["config"].as_table().unwrap() {
+        println!("cargo:rustc-cfg={}={}", name, value);
+    }
+
+    for (name, value) in config["feature"].as_table().unwrap() {
+        if let Some(active) = value.as_bool() {
+            if active {
+                println!("cargo:rustc-cfg=feature=\"{}\"", name);
+            }
+        }
+    }
 }
